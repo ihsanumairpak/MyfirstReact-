bd7e88c9e65994f92cb75d05dd043f81ca707e07
UMAIR IHSAN <33822464+ihsanumairpak@users.noreply.github.com>
2022-02-09 10:51:37 +0500

    Updates

---
+++ precache-manifest.5c7b57bfca76d1d9fc41ec3ec03dc2c1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28f448f66da98147af56e1c037250a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1803521fc25681ed6c6",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "d23e4357143bf8c9d49f",
+    "url": "/static/css/main.6fc8abd4.chunk.css"
+  },
+  {
+    "revision": "f1803521fc25681ed6c6",
+    "url": "/static/js/2.1466831c.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.1466831c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14aef561fe1d195dd373",
+    "url": "/static/js/3.852da101.chunk.js"
+  },
+  {
+    "revision": "d23e4357143bf8c9d49f",
+    "url": "/static/js/main.a8114cb2.chunk.js"
+  },
+  {
+    "revision": "0d264904caab9cdcb94a",
+    "url": "/static/js/runtime-main.826992a7.js"
+  },
+  {
+    "revision": "1fcdd9a74ecd6dbc4513e4e48bd2b0d2",
+    "url": "/static/media/img1.1fcdd9a7.jpeg"
+  },
+  {
+    "revision": "47e0d640e52c2674ece710c339ce668d",
+    "url": "/static/media/img2.47e0d640.jpeg"
+  },
+  {
+    "revision": "93b054e7d6f68e89d528ef62a3fa6b94",
+    "url": "/static/media/img3.93b054e7.jpeg"
+  },
+  {
+    "revision": "b37a6da43219f9669d4492f279e148fd",
+    "url": "/static/media/img4.b37a6da4.jpeg"
+  }
+]);
